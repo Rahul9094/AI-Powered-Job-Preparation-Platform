6481927e8ea660105b2d3559611f4224089583b8
Rahul Rajput <rahulsinhrajput9458@gmail.com>
2026-05-14 14:32:32 +0530

    final build fix

--- Fronted/src/features/auth/interview/pages/Home.jsx
+++ Fronted/src/features/auth/interview/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router';
-import { useInterview } from '../hooks/useinterview';
+// import { useInterview } from '../hooks/useinterview';
+import { useInterview } from "../hooks/useInterview";
 
 import './style/home.scss'
 function Home() {
